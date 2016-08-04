b7e2a1ef716e88cf3743138b06356d5d7b4c73c9
Chae Cramb <chaecramb@gmail.com>
2016-08-04 20:28:05 +0100

    refactor hamming to use count method

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -4,10 +4,8 @@
 
 object Hamming {
   def compute(strand1: String, strand2: String): Int = {
-    def countDifferences(diff: Int, nucs: (Char, Char)) = if (nucs._1 == nucs._2) diff else diff + 1
-
     if (strand1.length != strand2.length) throw new IllegalArgumentException
-    strand1.zip(strand2).foldLeft(0)(countDifferences)
+    strand1.zip(strand2).count(nucs => nucs._1 != nucs._2)
   }
 }
 
