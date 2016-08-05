0ec4f149abeabe69789e3ec028fbe451a3ecf3d1
Chae Cramb <chaecramb@gmail.com>
2016-08-05 08:33:09 +0100

    ruby pangram initial attempt

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,3 +1,14 @@
-class Pangram
+module BookKeeping
+  VERSION = 2 # Where the version number matches the one in the test.
+end
 
+class Pangram
+  ALPHA = [*("a".."z")]
+
+  def self.is_pangram?(str)
+    return false if str.empty?
+    test_str = str.downcase
+
+    ALPHA.reduce(false) {|_, l| test_str.include?(l) ? true : (return false)}
+  end
 end
