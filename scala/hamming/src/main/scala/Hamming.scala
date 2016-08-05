5fc9b4d4c1159d9f8c624dfcf59c860b0d1f2182
Chae Cramb <chaecramb@gmail.com>
2016-08-05 07:46:29 +0100

    uses require for parameter verification

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -4,7 +4,7 @@
 
 object Hamming {
   def compute(strand1: String, strand2: String): Int = {
-    if (strand1.length != strand2.length) throw new IllegalArgumentException
+    require (strand1.length == strand2.length, "Strands must be of the same length") 
     strand1.zip(strand2).count(nucs => nucs._1 != nucs._2)
   }
 }
