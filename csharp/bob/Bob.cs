874ee22b38d9a3ae521220d6cf5b7228e20061e9
Chae Cramb <chae.c@simul8.com>
2016-10-15 19:17:30 +0100

    initial csharp solutions

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+class Bob
+{
+    public static string Hey(string phrase)
+    {
+        phrase = phrase.Trim();
+        if (phrase.Length == 0)
+        {
+            return "Fine. Be that way!";
+        }
+        else if (phrase == phrase.ToUpper() && Regex.IsMatch(phrase, @"[A-Z]"))
+        {
+            return "Whoa, chill out!";
+        }
+        else if (phrase.Last() == '?')
+        {
+            return "Sure.";
+        }
+        return "Whatever.";
+    }
+}
+
---
+++ csharp/hello-world/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HelloWorld
+{
+    public static string Hello(string name)
+    {
+        return $"Hello, {name ?? "World"}!";
+    }
+}
