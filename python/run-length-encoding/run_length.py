6dc6998956845da8f0a56182ce18b97bab80f78b
Chae Cramb <chaecramb@gmail.com>
2016-11-22 06:20:09 +0000

    improve exercism default variable names

--- python/run-length-encoding/run_length.py
+++ python/run-length-encoding/run_length.py
@@ -1,12 +1,12 @@
-def encode(s):
-  runs = split_into_runs(s)
+def encode(original_string):
+  runs = split_into_runs(original_string)
   encoded_runs = encode_runs(runs)
   return ''.join(encoded_runs);
 
-def split_into_runs(s):
+def split_into_runs(original_string):
   runs = []
-  run = s[0]
-  for char in s[1:]:
+  run = original_string[0]
+  for char in original_string[1:]:
     if char == run[0]:
       run += char
     else:
@@ -18,8 +18,8 @@ def split_into_runs(s):
 def encode_runs(runs):
   return [r[0] if len(r) == 1 else str(len(r)) + r[0] for r in runs]
 
-def decode(s):
-  coded_runs = split_into_coded_runs(s)
+def decode(coded_strings):
+  coded_runs = split_into_coded_runs(coded_strings)
   return ''.join(coded_runs);
 
 def split_into_coded_runs(coded_string):
